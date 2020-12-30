5e3cad6f15a10c7c8bdd3c8d4670d7bddc254f32
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-30 22:18:43 +0600

    Update Program.cs

--- SaveTreeInFile/Program.cs
+++ SaveTreeInFile/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+//Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с
+//рекурсией и без.
 
 namespace SaveTreeInFile
 {
@@ -10,6 +13,29 @@ namespace SaveTreeInFile
     {
         static void Main(string[] args)
         {
+            string link = @"C:\";
+            StreamWriter userData = new StreamWriter("folders_and_files.txt", false);
+            Links(link, userData);
+            userData.Close();
+        }
+        static void Links(string rootFolder, StreamWriter stream)
+        {
+            string[] dirs = Directory.GetDirectories(rootFolder);
+            for (int i = 0; i<dirs.Length; i++)
+            {
+                stream.WriteLine(dirs[i]);
+                Files(dirs[i], stream);
+                Links(dirs[i], stream);
+            }
+        }
+        static void Files(string rootFolder, StreamWriter stream)
+        {
+            string[] files = Directory.GetFiles(rootFolder);
+            for (int i = 0; i<files.Length; i++)
+            {
+                stream.WriteLine(files[i]);
+            }
         }
     }
 }
+
